5b9de7f2c49cc835eb8da4830b7cd803262901da
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2021-07-20 17:54:58 +0530

    Implement the basic proxy logic

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,145 @@
-import '@yume-chan/adb-backend-webusb';
+import { AdbWebUsbBackend, AdbWebUsbBackendWatcher } from '@yume-chan/adb-backend-webusb';
 
-window.addEventListener("load", () => {
-    const header = document.createElement("h1");
-    header.innerText = "Webpack❤️TS"
-  
-    const body = document.querySelector("body");
-    body.appendChild(header);
-})
-  
+function onload() {
+    refreshDevices();
+
+    new AdbWebUsbBackendWatcher(refreshDevices);
+
+    document.querySelector("input#request").addEventListener("click", requestDevice);
+}
+
+async function requestDevice() {
+    await AdbWebUsbBackend.requestDevice();
+    refreshDevices();
+}
+
+async function refreshDevices() {
+    document.querySelector('div#devices').childNodes.forEach(c => c.remove());
+    const devices = await AdbWebUsbBackend.getDevices();
+    devices.forEach((device) => {
+        const div = document.createElement('div');
+
+        const span = document.createElement('span');
+        span.innerText = `${device.serial} (${device.name})`;
+        div.appendChild(span);
+
+        const button = document.createElement('input');
+        button.value = "Connect";
+        button.type = "button";
+        button.addEventListener('click', () => connectDevice(device));
+        div.appendChild(button);
+
+        document.querySelector('div#devices').prepend(div);
+    })
+}
+
+async function connectDevice(device: AdbWebUsbBackend) {
+    await device.connect();
+    console.log("connected", device);
+
+    let wsUrl = new URL(window.location.href);
+    wsUrl.protocol = wsUrl.protocol.replace('http', 'ws');
+    const ws = new WebSocket(wsUrl.href);
+
+    ws.onmessage = writeLoopCallback(device, ws);
+    readLoop(device, ws);
+}
+
+function getPayloadLength(header: ArrayBuffer) {
+    let view = new DataView(header);
+    return view.getUint32(12 /* byteOffset */, true /* littleEndian */);
+}
+
+async function readLoop(backend: AdbWebUsbBackend, ws: WebSocket) {
+    // Read header
+    let buffer: ArrayBuffer = await backend.read(24);
+
+    // Detect boundary
+    // Note that it relies on the backend to only return data from one write operation
+    while (buffer.byteLength !== 24) {
+        // Maybe it's a payload from last connection.
+        // Ignore and try again
+        buffer = await backend.read(24);
+    }
+    ws.send(buffer);
+
+    // let packetHeader = await parsePacketHeader(buffer, backend);
+    let payload_length = getPayloadLength(buffer); //packetHeader.payloadLength;
+
+    console.log("==> header", payload_length);
+
+    // Read payload as well
+    while (payload_length > 0) {
+        buffer = await backend.read(payload_length);
+        ws.send(buffer);
+
+        console.log(`==> payload ${payload_length} bytes`);
+        payload_length -= buffer.byteLength;
+    }
+
+    readLoop(backend, ws);
+}
+
+
+function writeLoopCallback(backend: AdbWebUsbBackend, ws: WebSocket): ((e: MessageEvent<any>) => any) {
+    const AWAITING_HEADER = "AWAITING_HEADER";
+    const AWAITING_PAYLOAD = "AWAITING_PAYLOAD";
+    let state = AWAITING_HEADER;
+    let payload_length = 0;
+
+    let lastPromise = Promise.resolve();
+
+    let handleEvent = async (data: Blob) => {
+        let buffer: ArrayBuffer;
+        
+        switch (state) {
+            case AWAITING_HEADER:
+                if (data.size < 24) {
+                    throw new Error("Error: Was Expecting at least 24 bytes");
+                }
+
+                buffer = await data.slice(0, 24).arrayBuffer();
+                backend.write(buffer);
+
+                // let packetHeader = await parsePacketHeader(buffer, backend);
+                payload_length = getPayloadLength(buffer); //packetHeader.payloadLength;
+
+                console.log("<== header", payload_length);
+
+                if (payload_length > 0) {
+                    state = AWAITING_PAYLOAD;
+                }
+
+                if (data.size > 24) {
+                    await handleEvent(data.slice(24));
+                }
+
+                break;
+            case AWAITING_PAYLOAD:
+                if (data.size > payload_length) {
+                    let boundry = payload_length;
+                    await handleEvent(data.slice(0, boundry));
+                    await handleEvent(data.slice(boundry));
+                }
+                else {
+                    buffer = await data.arrayBuffer();
+                    backend.write(buffer);
+                    console.log(`<== payload ${payload_length} bytes`);
+
+                    payload_length -= data.size;
+
+                    if (payload_length == 0) {
+                        state = AWAITING_HEADER;
+                    }
+                }
+
+                break;
+        }
+    }
+
+    return (event) => {
+        lastPromise = lastPromise.then(async () => { await handleEvent(event.data); });
+    }
+}
+
+window.addEventListener("load", onload);
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,37 @@
-let x: Number = 4
-console.log(x);
+import * as http from 'http';
+import * as net from 'net';
+import * as path from 'path';
+import * as express from 'express';
+import * as WebSocket from 'ws';
+
+const port = 3000;
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Serve static files from client
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Setup web socket server logic
+let wsStream: any;
+wss.on('connection', (ws: WebSocket) => {
+    console.log("Got a socket connection");
+    wsStream = WebSocket.createWebSocketStream(ws);
+    ws.on("open", () => console.log("OPEN"));
+    ws.on("close", () => console.log("CLOSED"));
+})
+
+net.createServer((socket: net.Socket) => {
+    if (!wsStream) {
+        socket.end();
+        return;
+    };
+
+    socket.pipe(wsStream);
+    wsStream.pipe(socket);
+
+    console.log("Got a connection on 5050");
+}).listen(5050);
+
+
+server.listen(port, () => console.log(`Started listening on http://localhost:${port}`));
