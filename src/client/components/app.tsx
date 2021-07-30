c10836738d210f9c693bf99d21f4f699a24d2217
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2021-07-30 12:26:58 +0530

    Use fluentui

--- src/client/components/app.tsx
+++ src/client/components/app.tsx
@@ -1,16 +1,24 @@
 import { useState, useEffect } from 'react';
+import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
+
 import { UsbDevice, monitorDevices, requestDevice } from '../usbDevices';
+import { Label, Separator, Stack, Text } from '@fluentui/react';
+
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+initializeIcons();
 
 function Device(props: {device: UsbDevice}) {
     let device: UsbDevice = props.device;
 
     return (
         <div>
-            {device.serial} {device.name}
-            {device.connected ? 
-                <input type="button" value="Disconnect" onClick={device.disconnect} /> :
-                <input type="button" value="Connect" onClick={device.connect} />
-            }
+            <Stack horizontal>
+                {device.connected ? 
+                    <DefaultButton text="Disconnect" onClick={device.disconnect} /> :
+                    <PrimaryButton text="Connect" onClick={device.connect} />
+                }
+                <Label style={{marginLeft: 32}}>{device.serial} - {device.name}</Label>
+            </Stack>
         </div>
     )
 }
@@ -23,11 +31,17 @@ export function App() {
     }, []);
 
     return (
-        <div>
-            <h1>Welcome</h1>
-            <input type="button" value="Add device" id="request" onClick={requestDevice} />
-            <div id="devices">
-                {devices.map((device) => (<Device device={device} key={device.serial} />))}
+        <div style={{maxWidth: 650, margin: "0 auto"}}>
+            {/* <Text variant="large">Welcome</Text> */}
+            <div style={{marginTop: 64}}>
+                <div style={{marginBottom: 48}}>
+                    <Text style={{marginRight: 12}}>Device not visible in the list below?</Text>
+                    <PrimaryButton text="Add device" id="request" onClick={requestDevice} iconProps={{iconName: "Add"}} />
+                </div>
+                <Separator>Connected Devices</Separator>
+                <Stack style={{marginTop: 12}}>
+                    {devices.map((device) => (<Device device={device} key={device.serial} />))}
+                </Stack>
             </div>
         </div>
     );
