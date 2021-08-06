b502c4c6a4dbf4f3946e8008a3a2f960b5affa7b
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2021-08-06 12:04:44 +0530

    Add args and https support

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,63 @@
 import * as http from 'http';
+import * as https from 'https';
 import * as net from 'net';
+import * as fs from 'fs';
 import * as path from 'path';
 import * as express from 'express';
 import * as WebSocket from 'ws';
 import { monitorAdbServer, addAdbDevice, removeAdbDevice } from './adbConnection';
 
+import * as yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+let argv = yargs(hideBin(process.argv))
+    .option('port', {
+        alias: 'p',
+        describe: 'port to run the server on',
+        default: 3000,
+        nargs: 1,
+        type: 'number',
+        group: 'Server Options:'
+    })
+    .option('key', {
+        describe: 'file containing RSA private key',
+        requiresArg: true
+    })
+    .option('cert', {
+        describe: 'file containing SSL certificate chain',
+        requiresArg: true
+    })
+    .implies({
+        'key': 'cert',
+        'cert': 'key'
+    })
+    .normalize(['key', 'cert'])
+    .check((argv) => {
+        if (argv.key) {
+            fs.accessSync(argv.key, fs.constants.R_OK);
+        }
+        if (argv.cert) {
+            fs.accessSync(argv.cert, fs.constants.R_OK);
+        }
+        return true;
+    })
+    .group(['key', 'cert'], "HTTPS Options:")
+    .usage('$0 [--port PORT] [--key server.key --cert server.crt]')
+    .help().alias('h', 'help')
+    .strict()
+    .parseSync()
+
+
+const useHttps = (argv.key && argv.cert);
+const httpsOptions = useHttps ? {
+    key: fs.readFileSync(argv.key),
+    cert: fs.readFileSync(argv.cert)
+} : {}
+
+//////////////////
 const port = 3000;
 const app = express();
-const server = http.createServer(app);
+const server = useHttps ? https.createServer(httpsOptions, app) : http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // Serve static files from client
@@ -47,5 +97,7 @@ wss.on('connection', (ws: WebSocket) => {
     });
 });
 
-server.listen(port, () => console.log(`Started listening on http://localhost:${port}`));
+server.listen(port, () => {
+    console.log(`Started listening on ${useHttps ? 'https' : 'http'}://localhost:${port}`);
+});
 monitorAdbServer();
