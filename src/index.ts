ca5d235bd5ee2da1cba0fc69e7a53d76576c4e34
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2022-08-22 12:15:02 +0530

    Decouple cmdline and lib in server, new server subcommand

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-require('./server');
+import * as server from './server/command';
+
+import * as yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+yargs(hideBin(process.argv))
+    .usage("$0 <command> [options]")
+    .command(server)
+    .help().alias('h', 'help')
+    .demandCommand(1, "")
+    .strict()
+    .parseSync()
---
+++ src/server/server.ts
@@ -0,0 +1,68 @@
+import * as http from 'http';
+import * as https from 'https';
+import * as net from 'net';
+import * as path from 'path';
+import * as express from 'express';
+import * as WebSocket from 'ws';
+
+import { monitorAdbServer, addAdbDevice, removeAdbDevice } from './adbConnection';
+
+export class Server {
+    private port: number;
+    private httpsOptions: https.ServerOptions;
+
+    constructor(port: number, httpsOptions?: https.ServerOptions) {
+        this.port = port;
+        this.httpsOptions = httpsOptions;
+    }
+
+    start() {
+        const app = express();
+        const useHttps = !!this.httpsOptions;
+        const server = useHttps ? https.createServer(this.httpsOptions, app) : http.createServer(app);
+        const wss = new WebSocket.Server({ server });
+
+        // Serve static files from client
+        app.use(express.static(path.join(__dirname, '../client')));
+
+        // Setup web socket server logic
+        let wsStream: any;
+        wss.on('connection', (ws: WebSocket) => {
+            console.log("Got a socket connection");
+            wsStream = WebSocket.createWebSocketStream(ws);
+            wsStream.on("error", () => {
+                // Do nothing
+                // Sometimes this can happen when trying to write to the socket after it is closed in the process of closing the connection. Ignore.
+            });
+
+            let port: Number;
+            let server = net.createServer((socket: net.Socket) => {
+                socket.pipe(wsStream, {end: false});
+                wsStream.pipe(socket);
+
+                socket.on("close", (hadError) => {
+                    socket.unpipe(wsStream);
+                    wsStream.unpipe(socket);
+                });
+            }).listen(0, () => {
+                port = (server.address() as net.AddressInfo).port;
+                console.log(port, "New device");
+
+                addAdbDevice(port);
+            });
+
+            ws.on("close", () => {
+                console.log(port, "Device lost");
+
+                server.close();
+                removeAdbDevice(port);
+            });
+        });
+
+        server.listen(this.port, () => {
+            console.log(`Started listening on ${useHttps ? 'https' : 'http'}://localhost:${this.port}`);
+        });
+
+        monitorAdbServer();
+    }
+}
