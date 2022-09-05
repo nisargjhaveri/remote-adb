0bd5d3ce1e6c985fc05d4922bd56080e6c53417f
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2022-09-05 23:30:59 +0530

    Add option to specify password when starting the server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as server from './server/command';
+import { commandServer } from './server/command';
 import { commandConnect, commandDevices } from './client/command';
 
 import yargs from 'yargs';
@@ -8,9 +8,12 @@ import { hideBin } from 'yargs/helpers';
 
 yargs(hideBin(process.argv))
     .usage("$0 <command> [options]")
-    .command(server)
+    .command(commandServer)
     .command(commandDevices)
     .command(commandConnect)
+    .parserConfiguration({
+        'duplicate-arguments-array': false,
+    })
     .help().alias('h', 'help')
     .demandCommand(1, "")
     .strict()
--- src/server/command.ts
+++ src/server/command.ts
@@ -1,55 +1,59 @@
 import * as fs from 'fs';
-import { Argv } from 'yargs';
+import { Argv, CommandModule } from 'yargs';
 
 import { Server } from './server';
 
+export const commandServer = {
+    command: "server",
+    describe: "Start in server mode",
+    builder: (yargs: Argv) => {
+        return yargs
+            .usage('$0 server [--port PORT] [--key server.key --cert server.crt]')
+            .option('port', {
+                alias: 'p',
+                describe: 'port to run the server on',
+                default: 3000,
+                nargs: 1,
+                type: 'number',
+            })
+            .option('password', {
+                describe: 'specify password required to connect',
+                type: 'string',
+                nargs: 1
+            })
+            .group(['port', 'password'], "Server Options:")
+            .option('key', {
+                describe: 'file containing RSA private key',
+                requiresArg: true
+            })
+            .option('cert', {
+                describe: 'file containing SSL certificate chain',
+                requiresArg: true
+            })
+            .implies({
+                'key': 'cert',
+                'cert': 'key'
+            })
+            .normalize(['key', 'cert'])
+            .check((argv) => {
+                if (argv.key) {
+                    fs.accessSync(argv.key, fs.constants.R_OK);
+                }
+                if (argv.cert) {
+                    fs.accessSync(argv.cert, fs.constants.R_OK);
+                }
+                return true;
+            })
+            .group(['key', 'cert'], "HTTPS Options:")
+            .strict()
+    },
+    handler: (args: {port?: number, key?: string, cert?: string, password?: string}) => {
+        const useHttps = !!(args.key && args.cert);
+        const httpsOptions = useHttps ? {
+            key: fs.readFileSync(args.key),
+            cert: fs.readFileSync(args.cert)
+        } : undefined;
 
-export const command = "server"
-export const describe = "Start in server mode"
-
-export function builder(yargs: Argv) {
-    return yargs
-        .usage('$0 server [--port PORT] [--key server.key --cert server.crt]')
-        .option('port', {
-            alias: 'p',
-            describe: 'port to run the server on',
-            default: 3000,
-            nargs: 1,
-            type: 'number',
-            group: 'Server Options:'
-        })
-        .option('key', {
-            describe: 'file containing RSA private key',
-            requiresArg: true
-        })
-        .option('cert', {
-            describe: 'file containing SSL certificate chain',
-            requiresArg: true
-        })
-        .implies({
-            'key': 'cert',
-            'cert': 'key'
-        })
-        .normalize(['key', 'cert'])
-        .check((argv) => {
-            if (argv.key) {
-                fs.accessSync(argv.key, fs.constants.R_OK);
-            }
-            if (argv.cert) {
-                fs.accessSync(argv.cert, fs.constants.R_OK);
-            }
-            return true;
-        })
-        .group(['key', 'cert'], "HTTPS Options:")
-        .strict()
-}
-
-export function handler(args: {port: number, key: string, cert: string}) {
-    const useHttps = !!(args.key && args.cert);
-    const httpsOptions = useHttps ? {
-        key: fs.readFileSync(args.key),
-        cert: fs.readFileSync(args.cert)
-    } : undefined;
-
-    new Server(args.port, httpsOptions).start();
-}
+        new Server(args.port, httpsOptions, args.password).start();
+    }
+} as CommandModule;
